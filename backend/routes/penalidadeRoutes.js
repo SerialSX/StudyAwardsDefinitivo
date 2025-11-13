55227e2238d21c3275ec029d94e60df89047b068
João Arthur <joaoarthurce@gmail.com>
2025-11-13 15:18:57 -0300

    Correção de error Middleware

--- backend/routes/penalidadeRoutes.js
+++ backend/routes/penalidadeRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
 const penalidadeController = require('../controllers/penalidadeController.js');
+const { authMiddleware, isProfessor } = require('../middleware/authMiddleware.js');
 
-router.get('/alunos/:id/penalidades', penalidadeController.getPenalidades);
-router.post('/registrar-falta', penalidadeController.registrarFalta);
-router.get('/verificar-atrasos', penalidadeController.verificarAtrasos);
+router.get('/alunos/:id/penalidades', authMiddleware, penalidadeController.getPenalidades);
+router.post('/registrar-falta', authMiddleware, isProfessor, penalidadeController.registrarFalta);
+router.get('/verificar-atrasos', authMiddleware, isProfessor, penalidadeController.verificarAtrasos);
 
 module.exports = router;
